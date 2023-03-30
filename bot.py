2732ed467610d61cb14a3eb55e316bf1857ef4a0
menthy-wu <wumengxuan0728@gmail.com>
2023-03-30 00:48:25 -0700

    add a comment

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ async def giveRole(ctx):
     member = GUILD.get_member(ctx)
     role = GUILD.get_role()
     await member.add_roles(role)
-
+#wow
 
 if __name__ == '__main__':
     bot.run(TOKEN)
