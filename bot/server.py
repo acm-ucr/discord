491adff8e2c1cc1497ad3aac4caf7fe39b4587ff
Falak Tulsi <falaktulsi@gmail.com>
2023-07-10 19:20:19 -0700

    Ran yapf format

--- bot/server.py
+++ bot/server.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:bot/server.py
 import os
 from dotenv import load_dotenv
 
@@ -44,51 +43,3 @@ class Server:
 
     def get_guild(self) -> int:
         return self.guild_id
-=======
-import os
-from dotenv import load_dotenv
-
-
-class Guild:
-
-    def __init__(self):
-        load_dotenv()
-        VERIFIED_ROLE = os.getenv('DISCORD_VERIFIED_ROLE')
-        ALUMNI_ROLE = os.getenv('DISCORD_ALUMNI_ROLE')
-        GRADUATE_ROLE = os.getenv('DISCORD_GRADUATE_ROLE')
-        UNDERGRADUATE_ROLE = os.getenv('DISCORD_UNDERGRADUATE_ROLE')
-        FACULTY_ROLE = os.getenv('DISCORD_FACULTY_ROLE')
-
-        GUILD = os.getenv('DISCORD_GUILD')
-
-        self.server = None
-        self.verified_role_id: int = int(VERIFIED_ROLE)
-        self.undergraduate_role_id = int(UNDERGRADUATE_ROLE)
-        self.graduate_role_id = int(GRADUATE_ROLE)
-        self.alumni_role_id = int(ALUMNI_ROLE)
-        self.faculty_role_id = int(FACULTY_ROLE)
-        self.guild_id: int = int(GUILD)
-
-    def get_member(self, ctx):
-        return self.server.get_member(ctx.user.id)
-
-    def get_roles(self, affiliation):
-        print(affiliation)
-        affiliation_role_id = 0
-        if affiliation == "undergraduate":
-            affiliation_role_id = self.undergraduate_role_id
-        elif affiliation == "graduate":
-            affiliation_role_id = self.graduate_role_id
-        elif affiliation == "alumni":
-            affiliation_role_id = self.alumni_role_id
-        elif affiliation == "faculty":
-            affiliation_role_id = self.faculty_role_id
-
-        return [
-            self.server.get_role(self.verified_role_id),
-            self.server.get_role(affiliation_role_id)
-        ]
-
-    def get_guild(self) -> int:
-        return self.guild_id
->>>>>>> 85a674828f59c36877545e7f78b29bac70976b0e:guild.py
